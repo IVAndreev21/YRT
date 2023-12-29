8a660014c5fc4857504de42863dd1df4a2357518
BKKyovtorov21 <BKKyovtorov21@codingburgas.bg>
2023-12-29 18:00:00 +0200

    choose file from os file dialog

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,12 +1,12 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
-MainWindow::MainWindow(const QString &username, QWidget *parent)
+MainWindow::MainWindow(const QString &username_ref, QWidget *parent)
     : QWidget(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-
+    username = username_ref;
     databaseManager = std::make_unique<DatabaseManager>();
     if(databaseManager->openConnection())
     {
@@ -41,10 +41,83 @@ MainWindow::~MainWindow()
 
 void MainWindow::updatepfp()
 {
-
 }
 void MainWindow::on_transactions_PB_clicked()
 {
+    ui->stackedWidget->setCurrentIndex(0);
+}
 
+
+void MainWindow::on_pfp_acc_PB_clicked()
+{
+    QString ImagePath = QFileDialog::getOpenFileName(this, tr("Select Image"), QCoreApplication::applicationDirPath(), tr("Image Files (*.jpg *.png)"), 0, QFileDialog::DontUseNativeDialog);
+
+    if (!ImagePath.isEmpty()) {
+        QPixmap Image(ImagePath);
+        QBuffer ImageBufferData;
+
+        if (ImageBufferData.open(QIODevice::ReadWrite))
+        {
+            QString fileExtension = QFileInfo(ImagePath).suffix().toLower();
+            if (fileExtension == "jpg" || fileExtension == "jpeg" || fileExtension == "png")
+            {
+                // Determine the image format based on the file extension
+                if (fileExtension == "jpg" || fileExtension == "jpeg")
+                    Image.save(&ImageBufferData, "JPG");
+                else if (fileExtension == "png")
+                    Image.save(&ImageBufferData, "PNG");
+
+                ImageBufferData.close();
+                QByteArray FinalDataToSave = ImageBufferData.buffer().toBase64();
+
+                if (databaseManager->openConnection())
+                {
+                    QSqlDatabase db = databaseManager->getDatabase();
+                    if (db.isValid() && db.isOpen())
+                    {
+                        QSqlQuery qry;
+                        qry.prepare("UPDATE users SET pfp = :pfp WHERE Username = :username");
+                        qry.bindValue(":pfp", FinalDataToSave);
+                        qry.bindValue(":username", username);
+                        if(qry.exec())
+                        {
+                            QSqlDatabase::database().commit();
+                            ui->pfp_acc_LA->setPixmap(Image);
+                            ui->pfp_acc_LA_2->setPixmap(Image);
+                        }
+                    }
+                }
+            }
+            else
+            {
+                // Handle unsupported file format here
+                QMessageBox::warning(this, tr("Unsupported Format"), tr("Only JPG and PNG files are supported."));
+            }
+        }
+    }
+}
+
+
+void MainWindow::on_payments_PB_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(1);
+}
+
+
+void MainWindow::on_cards_PB_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(2);
+}
+
+
+void MainWindow::on_account_PB_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(3);
+}
+
+
+void MainWindow::on_administration_PB_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(4);
 }
 
--- src/mainwindow.h
+++ src/mainwindow.h
@@ -2,6 +2,12 @@
 #define MAINWINDOW_H
 
 #include <QWidget>
+#include <QByteArray>
+#include <QFileInfo>
+#include <QPainter>
+#include <QPainterPath>
+#include <QMessageBox>
+#include <QFileDialog>
 #include "databasemanager.hpp"
 namespace Ui {
 class MainWindow;
@@ -12,17 +18,29 @@ class MainWindow : public QWidget
     Q_OBJECT
 
 public:
-    explicit MainWindow(const QString& username, QWidget *parent = nullptr);
+    explicit MainWindow(const QString& username_ref, QWidget *parent = nullptr);
     ~MainWindow();
 
 private slots:
     void on_transactions_PB_clicked();
+
+    void on_pfp_acc_PB_clicked();
+
+    void on_payments_PB_clicked();
+
+    void on_cards_PB_clicked();
+
+    void on_account_PB_clicked();
+
+    void on_administration_PB_clicked();
 
 private:
     Ui::MainWindow *ui;
     std::unique_ptr<DatabaseManager> databaseManager;
 
     void updatepfp();
+
+    QString username;
 };
 
 #endif // MAINWINDOW_H
